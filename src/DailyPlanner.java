a28cf519c32a40ae83c75a515b98d9c6bc07752d
Verkin78 <otrajenie.jc@gmail.com>
2022-12-21 14:35:00 +0300

    Kursach2 - Базовый уровень

---
+++ src/DailyPlanner.java
@@ -0,0 +1,117 @@
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class DailyPlanner {
+
+    private final String heading;
+    private String description;
+    private final Type taskType;
+    private final LocalDate localDate;
+    private Repeatability repeatability;
+
+    public DailyPlanner(String heading,
+                        String description,
+                        Type taskType,
+                        Repeatability repeatability,
+                        LocalDate localDate) {
+        if (heading == null) {
+            throw new IllegalArgumentException("Добавьте название!!!");
+        } else {
+            this.heading = heading;
+        }
+        setDescription(description);
+        if (taskType == null) {
+            throw new IllegalArgumentException("Тип задачи не указан!!!");
+        } else {
+            this.taskType = taskType;
+        }
+        this.localDate = localDate;
+        setRepeatability(repeatability);
+    }
+
+    public String getHeading() {
+        return heading;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        if (description == null) {
+            throw new IllegalArgumentException("Описание не заполнено");
+        } else {
+            this.description = description;
+        }
+    }
+
+    public Type getTaskType() {
+        return taskType;
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public Repeatability getRepeatability() {
+        return repeatability;
+    }
+
+    public void setRepeatability(Repeatability repeatability) {
+        if (repeatability == null) {
+            throw new IllegalArgumentException("Введите повторяемость!!!");
+        } else {
+            this.repeatability = repeatability;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DailyPlanner that = (DailyPlanner) o;
+        return heading.equals(that.heading) && description.equals(that.description) && taskType == that.taskType
+                && localDate.equals(that.localDate) && repeatability == that.repeatability;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(heading, description, taskType, localDate, repeatability);
+    }
+
+    @Override
+    public String toString() {
+        return "Задача -" + "\n" +
+                "Название - " + heading + '\n' +
+                "Описание - " + description + '\n' +
+                "Тип - " + taskType.getType() + '\n' +
+                "Дата - " + localDate + '\n' +
+                "Частота повторений - " + repeatability.getNameR();
+    }
+
+    public static void createDaily() {
+    }
+
+    public static void getDailyPlan(Map<DailyPlanner, Integer> planer) {
+        System.out.println("Введите дату: ");
+        Scanner scanner = new Scanner(System.in);
+        LocalDate date = LocalDate.of(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
+        System.out.println("Список задач на день6");
+        for (DailyPlanner planner : planer.keySet()) {
+            if (planner.getLocalDate().equals(date)) {
+                System.out.println(planer);
+            }
+        }
+    }
+
+    public static void delete(Map<DailyPlanner, Integer> planer) {
+        System.out.println("Введите значение для удаления");
+        Scanner scanner = new Scanner(System.in);
+        for (DailyPlanner planner : planer.keySet()) {
+            planer.remove(planner, scanner.nextInt());
+            System.out.println("Вы успешно удалил обьект ");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,54 @@
-import javax.swing.*;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            foo();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        Map<DailyPlanner, Integer> planer = new HashMap<>();
+        planer.put(new DailyPlanner("Курсовая", "Нужно сдать как можно скорее", Type.WORKED,
+                Repeatability.SINGLE,
+                LocalDate.of(2022, 12, 20)), 1);
+        try (Scanner scanner = new Scanner(System.in)) {
+            label:
+            while (true) {
+                printMenu();
+                System.out.print("Выберите пункт меню: ");
+                if (scanner.hasNextInt()) {
+                    int menu = scanner.nextInt();
+                    switch (menu) {
+                        case 1:
+                            inputTask(scanner);
+                            break;
+                        case 2:
+                            DailyPlanner.delete(planer);
+                            break;
+                        case 3:
+                            DailyPlanner.getDailyPlan(planer);
+                            break;
+                        case 0:
+                            break label;
+                    }
+                } else {
+                    scanner.next();
+                    System.out.println("Выберите пункт меню из списка!");
+                }
+            }
         }
     }
 
-    public static void foo() throws IOException, FileNotFoundException {
-
+    private static void inputTask(Scanner scanner) {
+        System.out.print("Введите название задачи: ");
+        String taskName = scanner.next();
+        System.out.println("Вы добавили задачу " + taskName);
     }
 
+    private static void printMenu() {
+        System.out.println(
+                "1. Добавить задачу" + '\n' +
+                        "2. Удалить задачу" + '\n' +
+                        "3. Получить задачу на указанный день" + '\n' +
+                        "0. Выход");
+    }
 }
 
