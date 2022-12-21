85e18787e328facb365a43bad54b818fa868ed88
Verkin78 <otrajenie.jc@gmail.com>
2022-12-21 14:03:06 +0300

    Kursach2 - task-8.2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        String string = (JOptionPane.showInputDialog(null, "Введите текст для строки"));
-        String s1 = string.substring(0, string.length() / 2);
-        String s2 = string.substring(string.length() / 2, string.length());
-        JOptionPane.showMessageDialog(null, "Введённая строка разделена пополам на (" + s1
-                + ") и (" + s2 + ")");
+        try {
+            foo();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public static void foo() throws IOException, FileNotFoundException {
+
+    }
+
 }
 
