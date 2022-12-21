b5f4145d1b69b25a6bbee796d6c0b6c5eb6fb222
Verkin78 <otrajenie.jc@gmail.com>
2022-12-21 13:28:35 +0300

    Kursach2 - task-8.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        String string = (JOptionPane.showInputDialog(null, "Введите текст для строки"));
+        String s1 = string.substring(0, string.length() / 2);
+        String s2 = string.substring(string.length() / 2, string.length());
+        JOptionPane.showMessageDialog(null, "Введённая строка разделена пополам на (" + s1
+                + ") и (" + s2 + ")");
     }
 }
+
